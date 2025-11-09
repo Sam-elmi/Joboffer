90694af93dfb1df08d61fc76f7cf306246985e2e
SAM elmi <samelmi@SAMs-MacBook-Pro.local>
2025-11-09 20:34:03 +0330

    Set Webapps in setting

--- WebClassProject/settings.py
+++ WebClassProject/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Account',
+    'Jobboffers',
+    'JobRequest',
+    'Resume',
+    'Userpanel',
 ]
 
 MIDDLEWARE = [
